318a9e2cf6bec727876338ce170a11131a6fa07b
unknown <Kristian.Correa001@mymdc.net>
2023-04-14 15:23:56 -0400

    test

---
+++ server/src/main/java/com/springpractice/employeemanager/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.springpractice.employeemanager.controller;
+
+import com.springpractice.employeemanager.model.Employee;
+import com.springpractice.employeemanager.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // get all employees
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+}
---
+++ server/src/main/java/com/springpractice/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.springpractice.employeemanager.repository;
+
+import com.springpractice.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
