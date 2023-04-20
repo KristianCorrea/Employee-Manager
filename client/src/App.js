962c006c9892404eafaeba17a9b7f73fc8333495
KristianCorrea <kristiangcorrea@live.com>
2023-04-20 00:58:36 -0400

    Added URL Component Routing functionality to client.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Routes, Route} from 'react-router';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
@@ -7,11 +9,16 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <div className="container">
-        <ListEmployeeComponent />
-      </div>
-      <FooterComponent />
+      <Router>
+          <HeaderComponent />
+            <div className="container">
+              <Routes>
+                <Route path="/" element = {<ListEmployeeComponent/>} />
+                <Route path="/employees" element = {<ListEmployeeComponent/>} />
+              </Routes>
+            </div>
+          <FooterComponent />
+      </Router>
     </div>
   );
 }
