ace732ca533c1af15bdc9ecda4e6d681e930fdba
Kristian <kristiangcorrea@live.com>
2023-04-24 18:58:34 -0400

    Employee CRUD Operations fully implemented.

--- server/src/main/java/com/springpractice/employeemanager/controller/EmployeeController.java
+++ server/src/main/java/com/springpractice/employeemanager/controller/EmployeeController.java
@@ -1,11 +1,16 @@
 package com.springpractice.employeemanager.controller;
 
+import com.springpractice.employeemanager.exceptions.ResourceNotFoundException;
 import com.springpractice.employeemanager.model.Employee;
 import com.springpractice.employeemanager.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 @CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
@@ -22,5 +27,30 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+    //get employee by id
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee with the ID of "+id+" not found."));
+        return ResponseEntity.ok(employee);
+    }
+    //update employee
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee updatedEmployee){
+        Employee employee = employeeRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee with the ID of "+id+" not found."));
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmailId(updatedEmployee.getEmailId());
+
+        return ResponseEntity.ok(employeeRepository.save(employee));
+    }
+    //delete employee
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee with the ID of "+id+" not found."));
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted, ", Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 }
